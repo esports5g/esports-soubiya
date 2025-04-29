148dd2416a5e1a95cb2f5a4781fc64de880e3222
esports5g <wang.esports5g@gmail.com>
2025-04-29 17:15:23 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-window.onload = () => {
-  document.getElementById('loader').style.display = 'none';
-  let index = 0;
-  const images = document.querySelectorAll('.carousel-img');
-  setInterval(() => {
-    images[index].classList.remove('active');
-    index = (index + 1) % images.length;
-    images[index].classList.add('active');
-  }, 3000);
-};
-particlesJS.load('particles-js', 'particles.json');
+// JavaScriptコードを書く場所
